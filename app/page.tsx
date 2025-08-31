a1caabff81b24cbd8d629194c3b49289c675ef8d
Chitose Ri <103087652@qq.com>
2025-08-31 13:04:08 -0400

    add footer component and integrate it into layout for improved site structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@
 
 import Image from 'next/image';
 import { useEffect, useRef } from 'react';
-import Link from "next/link";
-import InstagramIcon from "@/public/icons/Instagram";
 
 export default function HomePage() {
     const backgroundRef = useRef<HTMLDivElement>(null);
@@ -185,19 +183,6 @@ export default function HomePage() {
                             </section>
                         </main>
                     </div>
-                    <footer className="bg-black text-white text-center py-8 z-10">
-                        <div className="space-y-2">
-                            <div className="flex items-center justify-center">
-                                <span className="font-istok font-medium text-xl">
-                                    JIAYI TANG
-                                </span>
-                                <Link href="/" className="ml-2">
-                                    <InstagramIcon size={20} className="text-white"/>
-                                </Link>
-                            </div>
-                            <p className="text-gray-300">xxxxxxxxxx@gmail.com</p>
-                        </div>
-                    </footer>
                 </div>
             </div>
         </div>
